7da143dc3af5eac5dea54fa39c1114bc95cf2783
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-02-13 17:08:36 -0500

    initial setup complete...maybe minus some views

--- server.rb
+++ server.rb
@@ -4,6 +4,8 @@ require 'pry'
 require 'active_record'
 require_relative './lib/trip.rb'
 require_relative './lib/user.rb'
+require_relative './lib/bar.rb'
+require_relative './lib/stop.rb'
 
 configure do
 	enable :sessions
@@ -12,11 +14,13 @@ end
 
 # SPLASH
 get '/' do
+	File.read("./views/index.html")
 end
 
 # LOG IN WITH USER INFO AND VERIFY
-get '/users' do
+post '/session' do
 
+redirect '/users/:id'
 end
 
 # CREATE USER AND PERSIST IN DATABASE
@@ -32,22 +36,40 @@ delete '/users/:id' do
 end
 
 # SHOW ALL TRIPS FOR ONE USER
-get '/users/:id/trips' do
+get '/users/:id' do
+end
+
+# "FORM" FOR CREATING NEW TRIP
+get '/users/:id/trips/new' do
 end
 
 # SHOW SPECIFIC TRIP
-get '/users/:id/trips/:id' do
+get '/users/:user_id/trips/:id' do
 end
 
 # CREATE NEW TRIP AND PERSIST IN DATABASE
 post '/users/:id/trips' do
 end
 
-put '/users/:id/trips/:id' do
+# USERS DELETE A TRIP
+delete '/users/:user_id/trips/:id' do
 end
 
-delete '/users' do
+
+
+# /api_calls : SEARCH FOR ADDRESS
+# GET LAT LONG AS GEOCODE CALL
+get '/address_search/:address' do
 end
+
+# GET PLACES FROM GOOGLE PLACES API
+get '/places_search/:lat_long' do
+end
+
+# GET WEBSITE AND MAYBE OTHER DETAILS
+get '/details_search/:place_id' do
+end
+
 
 
 # /trips
