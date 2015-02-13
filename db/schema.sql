64938a140abad06be93621051e92ec060a9f4a54
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-02-13 15:52:18 -0500

    db schema (tables)

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,32 @@
-CREATE TABLE (
-	
+CREATE TABLE users(
+	id serial primary key,
+	f_name varchar(255),
+	l_name varchar(255),
+	email varchar(255),
+	password_digest varchar(255)
 	);
+
+CREATE TABLE trips(
+	id serial primary key,
+	user_id integer,
+	user_rating integer,
+	completed boolean,
+	time_created timestamp
+);
+
+CREATE TABLE bars(
+	id serial primary key,
+	name varchar(255),
+	address varchar(255),
+	website text,
+	pic_url text,
+	rating integer,
+	price_range integer
+);
+
+CREATE TABLE stops(
+	id serial primary key,
+	bar_id integer,
+	trip_id integer,
+	completed boolean
+);
