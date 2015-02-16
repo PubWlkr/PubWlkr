8728318c89863fcf3929fa21a061808132eb7b54
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-02-16 15:55:06 -0500

    see comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,17 @@
 console.log("LINKED DAWG");
+
+$("#sign_in").on("click", function () {
+	$("#login_form").attr("class", "hidden")
+	$("#new_user_form").attr("class", "visible")
+})
+
+
+
+
+$("#log_in").on("click", function () {
+	$("#new_user_form").attr("class", "hidden")
+	$("#login_form").attr("class", "visible")
+})
+
+
+
