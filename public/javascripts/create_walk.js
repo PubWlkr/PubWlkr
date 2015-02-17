cc346d06db0c111ff455dbf2b9a032385e6c744f
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-02-17 14:59:11 -0500

    createTrips function

--- public/javascripts/create_walk.js
+++ public/javascripts/create_walk.js
@@ -1,12 +1,12 @@
 console.log("connected!")
 
 var $genButton = $('#generate')
-
+var allDemBars;
 // generate new PubWlk on click of generate button
 $genButton.on('click', function(){
 	var address = $("#start").val();
 	var number_stops = $("#number_stops").val();
-	var radius = (parseFloat($("#radius").val()) * 1609.34)
+	var radius = (parseFloat($("#radius_input").val()) * 1609.34)
 	geocode(address, number_stops, radius);
 })
 
@@ -30,20 +30,74 @@ function barSearch(lat, lng, radius, number_stops){
 		url: "/places_search/" + lat + "/" + lng + "/" + radius,
 		type: "GET"
 	}).done(function (data){
-		var parsedData = JSON.parse(data)
-		var bars = parsedData.results.slice(0, parseInt(number_stops))
-		debugger
+		allDemBars = JSON.parse(data)
+		var bars = allDemBars.results.slice(0, parseInt(number_stops))
+		showTrip(bars)
 	})
 };
 
-// create trip, bars, and stops
-function createTrip(bars){
+// show trip, bars, and stops (confirmation page)
+function showTrip(bars) {
+	$("#container").empty()
+	var address_array = []
+	
+	for (var i = 0; i < bars.length; i++) {
 
+		var address = bars[i].vicinity;
+		address_array.push(address);
+		var name = bars[i].name;
+		var price_level = bars[i].price_level;
+		var rating = bars[i].rating;
+		var photo_reference = bars[i].photos[0].photo_reference;
+		var pic_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photo_reference + "&key=AIzaSyDKBnu8JwKe6sSLCuT6RK5PiCGUQbmbm_Q";
+		var place_id = bars[i].place_id;
+
+		cb(name, pic_url, address, price_level, rating, place_id)
+		// $.ajax({
+		// 	url: "/details_search/" + place_id,
+		// 	type: "GET"
+		// }).done(function (data) {
+		// 	var parsedData = JSON.parse(data);
+		// 	var website = parsedData.result.website;
+		// 	var barDiv = $("<div><h2>Name: " + name + "</h2><img src=" + pic_url + "><ul><li>" + address + "</li><li>" + website + "</li><li>Price Level: " + price_level + "</li><li>Average Rating: " + rating + "</li></ul></div>");
+
+		// 	$("#container").append(barDiv);
+		// })
+
+	}
 }
 
 
+function cb(name, pic_url, address, price_level, rating, place_id) {
+			$.ajax({ 
+			url: "/details_search/" + place_id,
+			type: "GET"
+		}).done(function (data) {
+			var parsedData = JSON.parse(data);
+			var website = parsedData.result.website;
+			var barDiv = $("<div><h2>Name: " + name + "</h2><img src=" + pic_url + "><ul><li>" + address + "</li><li>" + website + "</li><li>Price Level: " + price_level + "</li><li>Average Rating: " + rating + "</li></ul></div>");
+
+			$("#container").append(barDiv);
+		})
+}
+
+		// <div>
+		// 	<img src="{{pic_url}}">
+		// 	<h2>{{name}}</h2>
+		// 	<ul>
+		// 		<li>{{address}}</li>
+		// 		<li>{{website}}</li>
+		// 		<li>Price Level: {{price_level}}</li>
+		// 		<li>Average Rating: {{rating}}</li>
+		// 	</ul>
+		// </div>
 
 
+
+
+// create trip, bars, and stops; persist to database
+// function createTrip(bars){
+// }
 
 
 
