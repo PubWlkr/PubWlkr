87e42a22b8d5176b677460f5c460b3213e42fb9a
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-02-19 14:40:27 -0500

    loading gif and connection ifelse and images folder

--- db/connection.rb
+++ db/connection.rb
@@ -1,10 +1,15 @@
 require 'active_record'
 
-# ActiveRecord::Base.establish_connection({
-# 	:adapter => "postgresql",
-# 	:database => "pubwlkr"
-# })
+if ENV["DB_INFO"]
+	# when accessing in production
+	ActiveRecord::Base.establish_connection ('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/pubwlkr')
 
-ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/pubwlkr')
-
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+	ActiveRecord::Base.logger = Logger.new(STDOUT)
+else
+	# when accessing in development
+	ActiveRecord::Base.establish_connection ({
+		:adapter => "postgresql",
+		:database => "pubwlkr"
+	})
+	ActiveRecord::Base.logger = Logger.new(STDOUT)
+end
