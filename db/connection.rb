17ed1743bc568e2a43bdfbad652e84ea43719bfb
gretchenziegler <gretchenziegler@gmail.com>
2015-02-19 10:09:12 -0500

    added export db info to connection.rb for deployment

--- db/connection.rb
+++ db/connection.rb
@@ -1,8 +1,10 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-	:adapter => "postgresql",
-	:database => "pubwlkr"
-})
+# ActiveRecord::Base.establish_connection({
+# 	:adapter => "postgresql",
+# 	:database => "pubwlkr"
+# })
+
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"]  + '@127.0.0.1/pubwlkr')
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
