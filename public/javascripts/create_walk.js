aa1a8eb7accc9ee222d364d5f805a5e341ef5365
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-02-19 18:28:07 -0500

    buttons on login good, gasp and you've made bigger, columns wider on mypubwlks

--- public/javascripts/create_walk.js
+++ public/javascripts/create_walk.js
@@ -114,15 +114,19 @@ function domLoad(name, pic_url, address, price_level, rating, place_id) {
 		var waypoints = mapStrings.join("|")
 		map_url = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyDKBnu8JwKe6sSLCuT6RK5PiCGUQbmbm_Q&origin=" + origin + "&destination=" + destination + "&waypoints=" + waypoints
 
-    $iframe = $("<iframe width=400 height=400 frameborder='0' style='border:0' src='" + map_url + "'></iframe>")
+    $iframe = $("<div align='center'><iframe width=400 height=400 frameborder='0' style='border:0' src='" + map_url + "'></iframe></div>")
+
+    $clear_div = $("<div>");
+    $clear_div.attr("style", "clear:both;");
 
 
 		// append all the bars
 		var parsedData = JSON.parse(data);
 		var website = parsedData.result.website;
-		var barDiv = $("<div id ='" + place_id + "'><h2>Name: " + name + "</h2><img width=100 height=100 src=" + pic_url + "><ul><li>" + address + "</li><li><a href='" + website + "' target='_blank'>" + website + "</a></li><li>Price Level: " + price_level + "</li><li>Average Rating: " + rating + "</li></ul></div>");
+		var barDiv = $("<div class='wrap3' id ='" + place_id + "'><div class='bar-images'><img class='bar-photo' src=" + pic_url + "></div><div class='bar-info'><h3>" +name + "</h3><ul><li>" + address + "</li><li><a href='" + website + "' target='_blank'>" + website + "</a></li><li>Price Level: " + price_level + "</li><li>Average Rating: " + rating + "</li></ul></div><br></div><br><br>");
 
-		$("#container").append(barDiv);
+		$clear_div.append(barDiv);
+		$("#container").append($clear_div);
 	})
 
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,8 @@
 console.log("LINKED DAWG");
 
 $("#sign_in").on("click", function () {
+	$(this).css("background-color", "#f79727");
+	$("#log_in").css("background-color", "#c0c0c0")
 	$("#login_form").attr("class", "hidden")
 	$("#new_user_form").attr("class", "visible")
 })
@@ -9,6 +11,8 @@ $("#sign_in").on("click", function () {
 
 
 $("#log_in").on("click", function () {
+	$(this).css("background-color", "#f79727");
+	$("#sign_in").css("background-color", "#c0c0c0")
 	$("#new_user_form").attr("class", "hidden")
 	$("#login_form").attr("class", "visible")
 })
